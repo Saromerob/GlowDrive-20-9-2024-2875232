5e9a91c67a6d776f14347256cb3c16ce5baf1d49
Santiago Romero <163584681+Saromerob@users.noreply.github.com>
2024-07-29 16:01:58 -0500

    Agregacion de roles, correxion de errores en el encarpetamiento (#9)
    Co-authored-by: Saromerob <santiagomero2105@gmail.com>

--- views/Nosotros.php
+++ views/Nosotros.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Acerca de nosotros </title>
-		<link rel="stylesheet" type="text/css" href="styles/quienessomo.css">
-	</head>
-	<body>
-		<section class="about">
-				<div class="main">
-					<img src="../img/logo.jpeg" class="logo">
-						<div class="about-text"> 
-								<h1> Quiénes Somos</h1>
-								<h5>Desarolladores <span> & Diseñadores</span></h5>
-								<p> Somos un grupo de aprendices del SENA que se encuentran estudiando programación de Software y se nos ocurrió la idea
-										de hacer un aplicativo sencillo para aquellas personas </p><br><br>
-								<button type="button"> <a href="pag_inicio.php">Volver a inicio</a></button>
-						</div>
-				</div>
-		</section>
-	</body>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de nosotros </title>
+    <link rel="stylesheet" type="text/css" href="styles/quienessomo.css">
+</head>
+
+<body>
+    <section class="about">
+        <div class="main">
+            <img src="../img/logo.jpeg" class="logo">
+            <div class="about-text">
+                <h1> Quiénes Somos</h1>
+                <h5>Desarolladores <span> & Diseñadores</span></h5>
+                <p> Somos un grupo de aprendices del SENA que se encuentran estudiando programación de Software y se nos
+                    ocurrió la idea
+                    de hacer un aplicativo sencillo para aquellas personas </p><br><br>
+                <button type="button"><a href="
+                <?php
+					session_start();
+                    if ($_SESSION['role_id'] == 1) {
+                        echo 'admin/pag_inicio.php';
+                    } else {
+                        echo 'pag_inicio.php';
+                    }
+                ?>">Volver a inicio</a></button>
+            </div>
+        </div>
+    </section>
+</body>
+
 </html>
