07a5b3d77f2f8fb073efcc3c26b4848627d530bd
Aprendiz <Aprendiz@BOGAPRCPDFSP954.FormacionSena.RED>
2024-08-09 15:54:10 -0500

    Agregación de mapas

--- views/cliente/Mapa.php
+++ views/cliente/Mapa.php
@@ -53,9 +53,16 @@
 	</nav>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <div class="contenedor-principal">
-	<i> <P> <Font size="10"> <h1>Autolavados</h1> </Font>
-			<body bgcolor="F4D6FF">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.9890235928137!2d-74.11339912415592!3d4.595988542554673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f992512b0e255%3A0x109a96acffbb61c!2sSENA%20-%20Centro%20de%20Materiales%20y%20ensayos!5e0!3m2!1ses-419!2sco!4v1714676527213!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+	<center>
+		<i> <P> <Font size="10"> <h1>Autolavados</h1> </Font>
+		<body bgcolor="F4D6FF">
+			<h1>Autolavado Dacar's</h1>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9459.180121425268!2d-74.12985637784054!3d4.576897484993817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f98cd4271de4f%3A0xb851b08f62587206!2sAutolavado%20Dacar&#39;s!5e0!3m2!1ses-419!2sco!4v1723236096105!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
+			<h1>Moto lavado y Auto lavado Agua Car</h1>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9459.18516375047!2d-74.12243075148108!3d4.576515941230433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f98e781923d61%3A0x257a165c8a818641!2sMoto%20lavado%20y%20Auto%20lavado%20Agua%20Car!5e0!3m2!1ses-419!2sco!4v1723236534231!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
+			<h1>Auto Lavado Carwasn</h1>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9459.259751372729!2d-74.13083035574209!3d4.570868316673751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9f33623e7eff%3A0xee2c681d23f857ff!2sAuto%20Lavado%20Carwasn!5e0!3m2!1ses-419!2sco!4v1723236571849!5m2!1ses-419!2sco" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe><br>
+	</center>
 </div>
 <h1 class="info">
 	En este espacio se ubicaran todos los puntos 
