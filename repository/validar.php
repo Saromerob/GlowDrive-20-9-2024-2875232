07f30fe575587c26e40957cb33c4c12b0fc2074e
Santiago Romero <163584681+Saromerob@users.noreply.github.com>
2024-09-16 22:54:14 -0500

    Contraseña hasheada (#105)

--- repository/validar.php
+++ repository/validar.php
@@ -5,43 +5,47 @@ $conn = $db->conectar();
 
 session_start();
 $correo = $_POST['correo']; // Cambiar 'usuario' por 'correo'
-$contraseña = $_POST['contraseña'];
-
+$contrasena = $_POST['contrasena'];
 if ($conn) {
     // Arma el texto de la consulta.
-    $consulta = "SELECT * FROM usuarios WHERE correo = :correo AND contrasena = :contrasena";
+    $consulta = "SELECT * FROM usuarios WHERE correo = :correo";
     // Manda la consulta a la base de datos y agrega los parámetros (línea 15 y 16).
     $resultado = $conn->prepare($consulta);
     $resultado->bindValue(':correo', $correo); // Cambiar 'usuario' por 'correo'
-    $resultado->bindValue(':contrasena', $contraseña);
+    
     // Se ejecuta la consulta.
     $resultado->execute();
     // rowCount está contando las filas.
     $filas = $resultado->rowCount();
-
     if ($filas > 0) {
         // Obtener los datos del usuario
-        $userData = $resultado->fetch(PDO::FETCH_ASSOC);
-        // Guardar el rol en la sesión
-        $_SESSION['id'] = $userData['id'];
-        $_SESSION['role_id'] = $userData['role_id'];
-        $_SESSION['nombre'] = $userData['nombre'];
-        switch ($userData['role_id']) {
-            case 1:
-                header("Location: ../views/gerente/paginaInicio.php");
-                exit();
-                break;
-            case 2:
-                header("Location: ../views/cliente/paginaInicio.php");
-                break;
-            case 3:
-                header("Location: ../views/admin/paginaInicio.php");
-                exit();
-                break;
-            default:
-                $_SESSION['error_message'] = 'ERROR EN LA AUTENTICACIÓN';
-                header("Location: ../index.php");
-                exit();
+        $userData = $resultado->fetch(PDO::FETCH_ASSOC);        
+        if (password_verify($contrasena, $userData['contrasena'])) {
+             // Guardar el rol en la sesión
+            $_SESSION['id'] = $userData['id'];
+            $_SESSION['role_id'] = $userData['role_id'];
+            $_SESSION['nombre'] = $userData['nombre'];
+            switch ($userData['role_id']) {
+                case 1:
+                    header("Location: ../views/gerente/paginaInicio.php");
+                    exit();
+                    break;
+                case 2:
+                    header("Location: ../views/cliente/paginaInicio.php");
+                    break;
+                case 3:
+                    header("Location: ../views/admin/paginaInicio.php");
+                    exit();
+                    break;
+                default:
+                    $_SESSION['error_message'] = 'ERROR EN LA AUTENTICACIÓN';
+                    header("Location: ../index.php");
+                    exit();
+            }
+        } else {
+            $_SESSION['error_message'] = 'Correo o contraseña incorrectos';
+            header("Location: ../views/session/sesion.php");
+            exit();
         }
     } else {
         $_SESSION['error_message'] = 'Correo o contraseña incorrectos';
