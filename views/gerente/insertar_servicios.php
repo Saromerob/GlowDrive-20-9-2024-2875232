293153e0449c4ad84507f3eda0ab20ee4402bd32
Aprendiz <Aprendiz@BOGSISCPDGSD910.FormacionSena.RED>
2024-09-18 17:25:20 -0500

    estilos agregados

--- views/gerente/insertar_servicios.php
+++ views/gerente/insertar_servicios.php
@@ -41,6 +41,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Ejecutar la consulta
         if ($stmt->execute()) {
             echo "Servicio agregado exitosamente.";
+            header('location: agg_servicios.php ');
+            
         } else {
             echo "Error al agregar el servicio.";
         }
